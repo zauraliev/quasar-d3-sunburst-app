f902ff8187eeff8788b0f53a7060fd168d040931
Zaur <zaur_aliev@hotmail.com>
2020-11-06 17:03:50 -0500

    Adding initial project files

---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <sunburst-chart></sunburst-chart>
+  </q-page>
+</template>
+
+<script>
+import SunburstChart from 'components/SunburstChart.vue'
+export default {
+  name: 'PageIndex',
+  components: { SunburstChart },
+}
+</script>
